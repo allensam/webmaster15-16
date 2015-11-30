3f68c095b396e7f658be380cf5e7e773af3c5684
WhiteDeath820 <allensam0@gmail.com>
2015-11-29 23:33:42 -0700

    did lots of css work and some routing

---
+++ src/js/app.js
@@ -0,0 +1,36 @@
+var website = angular.module('app', ['app.controllers', 'ui.router']);
+
+
+website.run(function ($state, $rootScope, $stateParams) {
+    //makes states work with html5
+    $rootScope.$state = $state;
+    $rootScope.$stateParams = $stateParams;
+    $state.go('home')
+});
+
+website.config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
+    //enables html5 mode
+    $locationProvider
+        .html5Mode(
+        {
+            enabled: true,
+            requireBase: false
+        })
+        .hashPrefix('!');
+
+    //states
+    $stateProvider
+
+        .state('home', {
+            url: '/',
+            templateUrl: 'templates/home.html'
+        })
+        .state('drones', {
+            url: '/learn',
+            templateUrl: 'templates/drones.html'
+        })
+        .state('mvhs', {
+            url: '/mvhs',
+            templateUrl: 'templates/mvhs.html'
+        });
+});
